21a31dd78403b8bfbce4fe030420fd8aa19a055c
Bruno <engenheiro.bgr@gmail.com>
2026-04-16 08:09:35 -0300

    Bump SW v13 + version string to force full cache refresh

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'sistema-vida-v12';
+const CACHE_NAME = 'sistema-vida-v13';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
-    './app.js?v=20260415-auth-storage-sync',
+    './app.js?v=20260415-image-autosync',
     './views/hoje.html',
     './views/planos.html',
     './views/proposito.html',
